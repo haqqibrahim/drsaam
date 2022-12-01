f2b2198148c7c7ee84fa94a6c8662627c413e376
haqqibrahim <enessyibrahim@gmail.com>
2022-12-01 09:54:50 +0100

    Signup&hashpassword

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,33 @@
 // Model
-const User = require("../models/userModel")
+const User = require("../models/userModel");
+
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({mssg: "Login User"})
-}
-
+  res.json({ mssg: "Login User" });
+};
 
 // Signup user
 const signupUser = async (req, res) => {
-    res.json({mssg: "Signup User"})
-}
+  function randomString(length, chars) {
+    var result = "";
+    for (var i = length; i > 0; --i)
+      result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+  }
 
+  const { email, username, phone, password } = req.body;
+  const uid = randomString(
+    5,
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  );
 
-module.exports = {loginUser, signupUser}
+  try {
+    const user = await User.signup(email, username, phone, password, uid);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.messagge });
+  }
+};
+
+module.exports = { loginUser, signupUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,69 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    phone: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  uid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
 
-module.exports = mongoose.model("User", userSchema)
+// Static signup method
+userSchema.statics.signup = async function (
+  email,
+  username,
+  phone,
+  password,
+  uid
+) {
+  const emailExists = await this.findOne({ email });
+  const usernameExists = await this.findOne({ username });
+  const phoneExists = await this.findOne({ phone });
+
+  if (emailExists) {
+    throw Error("oops, email already exist");
+  }
+  if (usernameExists) {
+    throw Error("Yikes, username already exist");
+  }
+  if (phoneExists) {
+    throw Error("oops, phone number already exist");
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    email,
+    username,
+    phone,
+    uid,
+    password: hash,
+  });
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
