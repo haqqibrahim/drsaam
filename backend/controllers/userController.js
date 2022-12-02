ddb45bcca5ba376853d9b8ebc0545a0a2c7a5c15
haqqibrahim <enessyibrahim@gmail.com>
2022-12-02 01:46:30 +0100

    jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,13 @@
 // Model
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken")
 
+// Create Token function
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {
+    expiresIn: "3d"
+  })
+}
 
 // login user
 const loginUser = async (req, res) => {
@@ -24,7 +31,10 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, username, phone, password, uid);
-    res.status(200).json({ email, user });
+
+    // create a token
+    const token = createToken(user._id)
+    res.status(200).json({ email, token });
   } catch (error) {
     console.log(error)
     res.status(400).json({ error: error.message });
