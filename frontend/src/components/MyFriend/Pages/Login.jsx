c73d197f461f49ce81a7606b7b2cffc40e0906dc
haqqibrahim <enessyibrahim@gmail.com>
2023-02-16 08:03:10 +0100

    MYFriendLogin

--- frontend/src/components/MyFriend/Pages/Login.jsx
+++ frontend/src/components/MyFriend/Pages/Login.jsx
@@ -56,16 +56,16 @@ const SignIn = () => {
       await signInWithEmailAndPassword(auth, email, password);
       setSucc(true);
       setErr("");
-      // await updateProfile(res.user, {
-      //   displayNamey77
-      // });
-      // await setDoc(doc(db, "myfriend", res.user.uid), {
-      //   status: "myfriend",
-      //   displayName,
-      //   uid: res.user.uid,
-      //   bio: ""
-      // });
-      // await setDoc(doc(db, "usersChat", res.user.uid), {});
+      await updateProfile(res.user, {
+        displayName
+      });
+      await setDoc(doc(db, "myfriend", res.user.uid), {
+        status: "myfriend",
+        displayName,
+        uid: res.user.uid,
+        bio: ""
+      });
+      await setDoc(doc(db, "usersChat", res.user.uid), {});
       setErr(false);
       navigate("/myfriend/home");
     } catch (err) {
